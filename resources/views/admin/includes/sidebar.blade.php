e6fb51bf0d82ebf646a5fa098b17c5c6cce66b55
s1llhoutte <danikval2000002@gmail.com>
2023-09-27 14:14:29 +0300

    crud for posts

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -6,6 +6,15 @@
         <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
             <li class="nav-item">
+                <a href="{{ route('admin.post.index') }}" class="nav-link">
+                    <i class="nav-icon far fa-clipboard"></i>
+                    <p>
+                        Посты
+                    </p>
+                </a>
+            </li>
+
+            <li class="nav-item">
                 <a href="{{ route('admin.category.index') }}" class="nav-link">
                     <i class=" nav-icon fas fa-th-list"></i>
                     <p>
@@ -22,6 +31,7 @@
                     </p>
                 </a>
             </li>
+
 
         </ul>
 
