9e15d5fb4e035947d17b696892b74d3aa8c7c19e
s1llhoutte <danikval2000002@gmail.com>
2023-09-28 18:40:58 +0300

    update postov

--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -1,4 +1,4 @@
-\@extends('admin.layouts.main')
+@extends('admin.layouts.main')
 @section('content')
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
@@ -26,16 +26,83 @@
             <!-- Small boxes (Stat box) -->
             <div class="row">
                 <div class="col-12">
-                    <form action="{{ route('admin.post.update', $post->id) }}" method="post" class="w-25">
+                    <form action="{{ route('admin.post.update', $post->id) }}" method="post" enctype="multipart/form-data">
                         @csrf
                         @method('patch')
-                        <div class="form-group">
-                            <input type="text" class="form-control" name="title" placeholder="Название поста" value="{{ $post->title }}">
+                        <div class="form-group w-25">
+                            <input type="text" class="form-control" name="title" placeholder="Название поста"
+                                   value="{{ $post->title }}">
                             @error('title')
-                                <div class="text-danger">Это поле необходимо для заполнения</div>
+                            <div class="text-danger">Это поле необходимо для заполнения</div>
                             @enderror
                         </div>
-                        <input type="submit" name="btn btn-block btn-primary" value="Обновить">
+                        <div class="form-group">
+                            <textarea id="summernote" name="content">
+                                 {{ $post->content }}
+                            </textarea>
+                            @error('content')
+                            <div class="text-danger">Это поле необходимо для заполнения</div>
+                            @enderror
+                        </div>
+                        <div class="form-group w-50">
+                            <label for="exampleInputFile">Добавить превью</label>
+                            <div class="w-25 mb-2">
+                                <img src="{{ url('storage/' . $post->preview_image) }}" alt="preview_image" class="w-50">
+                            </div>
+                            <div class="input-group">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" name="preview_image">
+                                    <label class="custom-file-label">Выберите изображение</label>
+                                </div>
+                                <div class="input-group-append">
+                                    <span class="input-group-text">Загрузка</span>
+                                </div>
+                            </div>
+                            @error('preview_image')
+                            <div class="text-danger">Это поле необходимо для заполнения</div>
+                            @enderror
+                        </div>
+                        <div class="form-group w-50">
+                            <label for="exampleInputFile">Добавить главное изображение</label>
+                            <div class="w-50 mb-2">
+                                <img src="{{ url('storage/' . $post->main_image) }}" alt="main_image" class="w-50">
+                            </div>
+                            <div class="input-group">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" name="main_image">
+                                    <label class="custom-file-label">Выберите изображение</label>
+                                </div>
+                                <div class="input-group-append">
+                                    <span class="input-group-text">Загрузка</span>
+                                </div>
+                            </div>
+                            @error('main_image')
+                            <div class="text-danger">Это поле необходимо для заполнения</div>
+                            @enderror
+                        </div>
+                        <div class="form-group w-50">
+                            <label>Выберите категорию</label>
+                            <select name="category_id" class="form-control">
+                                @foreach($categories as $category)
+                                    <option value="{{ $category->id }}"
+                                        {{ $post->category_id == $category->id ? 'selected' : '' }}>
+                                        {{ $category->title }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>Тэги</label>
+                            <select class="select2" name="tag_ids[]" multiple="multiple" data-placeholder="Выберите тэги"
+                                    style="width: 100%;">
+                                @foreach($tags as $tag)
+                                    <option {{ is_array( $post->tags->pluck('id')->toArray() ) && in_array($tag->id, $post->tags->pluck('id')->toArray() ) ? ' selected' : '' }} value="{{ $tag->id }}">{{ $tag->title }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" name="btn btn-block btn-primary" value="Обновить">
+                        </div>
                     </form>
                 </div>
                 <!-- ./col -->
