4576a947b31600b8f4cfd7c4447cd588dbe8dd7d
s1llhoutte <danikval2000002@gmail.com>
2024-07-23 13:17:36 +0300

    + adminLte & fix podklychenii edici

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,10 @@ Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
     Route::get('/', 'IndexController')->name('index');
 });
 
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', 'IndexController');
+    });
+});
+
 Auth::routes();
