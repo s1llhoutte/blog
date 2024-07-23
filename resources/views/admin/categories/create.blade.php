de18cdafd8ab84ea480f8fe6edb8bc123198f411
s1llhoutte <danikval2000002@gmail.com>
2024-07-23 15:14:01 +0300

    backend for create categories

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -26,9 +26,13 @@
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
                     <div class="col-12">
-                        <form action="#" class="w-25">
+                        <form action="{{ route('admin.category.store') }}" method="POST" class="w-25">
+                            @csrf
                             <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Название категории">
+                                <input type="text" class="form-control" name="title" placeholder="Название категории">
+                                @error('title')
+                                    <div class="text-danger">Это поле необходимо заполнить !</div>
+                                @enderror
                             </div>
                             <input type="submit" class="btn btn-primary" value="Добавить">
                         </form>
