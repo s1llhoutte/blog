e3d01178990ef8ba5bce5a32295cbe52e2595538
s1llhoutte <danikval2000002@gmail.com>
2024-07-29 12:11:22 +0300

    pt.3 crud for posts - add fields "preview_image" && "main_image"

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -70,6 +70,7 @@
 </script>
 <script src="{{ asset('plugins/moment/moment.min.js') }}"></script>
 <script src="{{ asset('plugins/daterangepicker/daterangepicker.js') }}"></script>
+<script src="{{ asset('plugins/bs-custom-file-input/bs-custom-file-input.min.js') }}"></script>
 <script src="{{ asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
 <script src="{{ asset('dist/js/adminlte.js') }}"></script>
 <script>
@@ -85,6 +86,11 @@
                 ['height', ['height']]
             ]
         });
+    });
+</script>
+<script>
+    $(function () {
+        bsCustomFileInput.init();
     });
 </script>
 </body>
