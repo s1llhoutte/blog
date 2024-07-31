96c6d97edb869a8daaba68ca166549807b6cce0a
s1llhoutte <danikval2000002@gmail.com>
2024-07-31 13:06:28 +0300

    pt.4 crud for users - add middleware for admin

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
     Route::get('/', 'IndexController')->name('index');
 });
 
-Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin', 'middleware' => [ 'admin']], function () {
     Route::group(['namespace' => 'Main'], function () {
         Route::get('/', 'IndexController');
     });
