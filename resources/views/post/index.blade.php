b8dc8ce5c75b6dcc39ed7987256be39958bda86e
s1llhoutte <danikval2000002@gmail.com>
2024-08-08 13:42:45 +0300

    adding likes to post

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -11,7 +11,24 @@
                             <div class="blog-post-thumbnail-wrapper">
                                 <img src="{{ 'storage/' . $post->preview_image }}" alt="blog post">
                             </div>
-                            <p class="blog-post-category">{{ $post->category->title }}</p>
+                            <div class="d-flex justify-content-between">
+                                <p class="blog-post-category">{{ $post->category->title }}</p>
+                                @auth
+                                    <form action="{{ route('post.like.store', $post->id) }}" method="post">
+                                        @csrf
+                                        <span>{{ $post->liked_users_count }}</span>
+                                        <button type="submit" class="border-0 bg-transparent">
+                                                <i class="fa{{ auth()->user()->likedPosts->contains($post->id) ? 's' : 'r'}} fa-heart"></i>
+                                        </button>
+                                    </form>
+                                @endauth
+                                @guest()
+                                    <div>
+                                        <span>{{ $post->liked_users_count }}</span>
+                                        <i class="far fa-heart"></i>
+                                    </div>
+                                @endguest
+                            </div>
                             <a href="{{ route('post.show', $post->id) }}" class="blog-post-permalink">
                                 <h6 class="blog-post-title">{{ $post->title }}</h6>
                             </a>
